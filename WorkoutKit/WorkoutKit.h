2075d08f2c04399e42b0b3d57d649cb28c4351fe
Benjamin Payen <payen.benjamin@gmail.com>
2017-05-05 00:22:53 +0200

    Add xcode project

---
+++ WorkoutKit/WorkoutKit.h
@@ -0,0 +1,19 @@
+//
+//  WorkoutKit.h
+//  WorkoutKit
+//
+//  Created by Benjamin on 05/05/2017.
+//  Copyright © 2017 Benjamin Payen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WorkoutKit.
+FOUNDATION_EXPORT double WorkoutKitVersionNumber;
+
+//! Project version string for WorkoutKit.
+FOUNDATION_EXPORT const unsigned char WorkoutKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkoutKit/PublicHeader.h>
+
+
